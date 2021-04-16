8925f9229d1061bae85597c082d9dee791d8dc9e
Rameenh <Rameenh@live.com>
2021-04-16 17:18:20 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/scratchpad.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scratchpad.dir/main.cpp.o"
+  "libscratchpad.dll.a"
+  "scratchpad.exe"
+  "scratchpad.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scratchpad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    default_random_engine generator (time(NULL));
+    poisson_distribution<int> distribution (10);
+
+    int pageNumbers [1000] = {};
+
+    for (int i=0; i<1000; ++i){
+        pageNumbers[i] = distribution(generator); //
+    }
+
+    return 0;
+}
