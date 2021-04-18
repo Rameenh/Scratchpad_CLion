3d53fb7016cbecee76ed40b493d6ddbda32025ec
Rameenh <Rameenh@live.com>
2021-04-17 22:24:28 -0500

    Removed unnecessary include created print queue method

--- main.cpp
+++ main.cpp
@@ -1,20 +1,55 @@
 
 #include <iostream>
-#include <random>
-#include <time.h>
+#include <queue>
 
 using namespace std;
 
-int main()
-{
-    default_random_engine generator (time(NULL));
-    poisson_distribution<int> distribution (10);
-
-    int pageNumbers [1000] = {};
-
-    for (int i=0; i<1000; ++i){
-        pageNumbers[i] = distribution(generator); //
+bool queueContains(queue<int> a, int b) {
+    for (int i = a.size(); i > 0; i--) {
+        if (a.front() == b) {
+            return true;
+        }
+        a.pop();
     }
+    return false;
+}
 
+void print_queue(queue<int> q) {
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+    cout << endl;
+}
+
+int main() {
+
+    queue<int> FIFOqueue;
+    int queueSize = 0;
+    FIFOqueue.push(2);
+    FIFOqueue.push(3);
+    FIFOqueue.pop();
+    FIFOqueue.push(2);
+    FIFOqueue.push(1);
+    print_queue(FIFOqueue);
+    FIFOqueue.pop();
+    FIFOqueue.push(5);
+    print_queue(FIFOqueue);
+    FIFOqueue.pop();
+    FIFOqueue.push(2);
+    FIFOqueue.pop();
+    FIFOqueue.push(4);
+    print_queue(FIFOqueue);
+    FIFOqueue.pop();
+    FIFOqueue.push(5);
+    FIFOqueue.pop();
+    FIFOqueue.push(3);
+    print_queue(FIFOqueue);
+    FIFOqueue.pop();
+    FIFOqueue.push(2);
+    print_queue(FIFOqueue);
+    FIFOqueue.pop();
+    FIFOqueue.push(5);
     return 0;
 }
